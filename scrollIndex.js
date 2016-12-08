2bc968b3e92ec6f6580e1adf4dcc0c1096290c94
dmaronnier <dimitri.maronnier@etu.montpellier.fr>
2016-12-08 12:37:42 +0100

    footer+useful Link area

--- scrollIndex.js
+++ scrollIndex.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$("#test").animate({
-            left: '250px',
+
             height: '+=150px',
             width: '+=150px'
         }).delay( 800 ).animate({
-            left: '250px',
+
             height: '-=150px',
             width: '-=150px'
         });
