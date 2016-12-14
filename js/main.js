4ed903b0e6562827810c24f61e458a5266f01181
Thomas Rampin <rampinthomas@gmail.com>
2016-12-14 01:34:08 +0100

    Refonte du css, menu ok, et responsive. Prochaine etape carroussel et regler les probleme de décalage entre les différents container notament "useful links" et les articles, cf utiliser les colonnes de bootsrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    /* Pour que nos menus se déroulent lorsque la souris passe dessus */
+    if (window.innerWidth > 767){
+        $('.dropdown').hover(function(){
+            $('.dropdown-toggle', this).trigger('click');
+        });
+    }
+
+
 	$("#test").animate({
 
             height: '+=150px',
